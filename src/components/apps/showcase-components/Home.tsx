a468011bef37dc3e721f6bf1fd10ea0d291c1bb4
iammix <Konstantinos Mixios>
2024-07-18 14:59:51 +0300

    UPDATE

--- src/components/apps/showcase-components/Home.tsx
+++ src/components/apps/showcase-components/Home.tsx
@@ -14,6 +14,7 @@ const Home: React.FC<HomeProps> = (props) => {
             </div>
             <div style={styles.buttons}>
                 <Link containerStyle={styles.link} to='about' text='ABOUT' />
+                <Link containerStyle={styles.link} to='contact' text='CONTACT'/>
 
             </div>
             {/*<div style={styles.buttons}>*/}
